564eee437d8a2b7267ffee76c9fc241bf445a10f
Kazberovich <kazbers@gmail.com>
2014-03-25 16:44:16 +0300

    little update

--- KSVKPlayer/Models/KSAudio.m
+++ KSVKPlayer/Models/KSAudio.m
@@ -35,8 +35,8 @@
     if (self)
     {
         self.aid = [responseObject objectForKey:@"aid"];
-        self.title = [[responseObject objectForKey:@"title"] stringByReplacingOccurrencesOfString: @"&amp;" withString: @"&"];
-        self.artist = [[responseObject objectForKey:@"artist"] stringByReplacingOccurrencesOfString: @"&amp;" withString: @"&"];
+        self.title = [[responseObject objectForKey:@"title"] stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
+        self.artist = [[responseObject objectForKey:@"artist"] stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
         self.genre = [responseObject objectForKey:@"genre"];
         self.duration = [responseObject objectForKey:@"duration"];
         self.url = [responseObject objectForKey:@"url"];        
